eca964adfeb5e81fe2ad6abda11d8245e4910904
KEI <samartketrahong@gmail.com>
2021-02-18 20:39:46 +0700

    version 18/02/64 20.37 - update front end login page

--- lib/Login/LoginMain.dart
+++ lib/Login/LoginMain.dart
@@ -8,73 +8,77 @@ class LoginMain extends StatefulWidget {
 }
 
 class LoginState extends State<LoginMain> {
+  FocusNode usernameNode;
+  FocusNode passwordNode;
+  FocusNode loginNode;
+
+  @override
+  void initState() {
+    usernameNode = FocusNode();
+    passwordNode = FocusNode();
+    loginNode = FocusNode();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: ColorLogin.bg,
-      body: LoginContant(),
-    );
-  }
-}
-
-class LoginContant extends StatelessWidget {
-  const LoginContant({
-    Key key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      margin: MediaQuery.of(context).padding,
-      color: ColorLogin.bg,
-      child: Padding(
-        padding: EdgeInsets.fromLTRB(MediaQuery.of(context).size.width * 0.05, 0,
-            MediaQuery.of(context).size.width * 0.05, 0),
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: [
-            TextEvent(
-              text: '> ข้าม',
-              Goto: () => {},
-            ),
-
-            ImageLogo(),
-            TextAccount(IconCheng: Icons.account_circle),
-            Space(
-              EmtySpace: MediaQuery.of(context).size.height * 0.01,
-            ),
-            TextAccount(IconCheng: Icons.lock),
-            Space(
-              EmtySpace: MediaQuery.of(context).size.height * 0.01,
-            ),
-            ButtonAccessSystem(Goto: ()=>{},),
-            TextRegis(),
-            Space(
-              EmtySpace: MediaQuery.of(context).size.height * 0.03,
-            ),
-            TextOr(),
-            Space(
-              EmtySpace: MediaQuery.of(context).size.height * 0.05,
-            ),
-            ButtonAccess(
+      body: Container(
+        margin: MediaQuery.of(context).padding,
+        color: ColorLogin.bg,
+        child: Padding(
+          padding: EdgeInsets.fromLTRB(MediaQuery.of(context).size.width * 0.05, 0,
+              MediaQuery.of(context).size.width * 0.05, 0),
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: [
+              TextEvent(
+                text: '> ข้าม',
+                Goto: () => {},
+              ),
+              ImageLogo(),
+              Space(
+                EmtySpace: MediaQuery.of(context).size.height * 0.05,
+              ),
+              TextAccount(IconCheng: Icons.account_circle,nextnode: passwordNode , mynode: usernameNode,autofocus: true,hintext: 'บัญชีผู้ใช้งาน',),
+              Space(
+                EmtySpace: MediaQuery.of(context).size.height * 0.02,
+              ),
+              TextAccount(IconCheng: Icons.lock,nextnode: loginNode,mynode: passwordNode,autofocus: false,hintext: 'รหัสผ่าน',),
+              Space(
+                EmtySpace: MediaQuery.of(context).size.height * 0.02,
+              ),
+              ButtonAccessSystem(Goto: ()=>{},mynode: loginNode,),
+              TextRegis(),
+              Space(
+                EmtySpace: MediaQuery.of(context).size.height * 0.03,
+              ),
+              TextOr(),
+              Space(
+                EmtySpace: MediaQuery.of(context).size.height * 0.05,
+              ),
+              ButtonAccess(
+                  Goto: ()=>{},
+                  AccessText: 'เข้าสู่ระบบด้วย Google',
+                  ButColor: Color.fromRGBO(245, 9, 9, 10)),
+              Space(EmtySpace: MediaQuery.of(context).size.height * 0.02),
+              ButtonAccess(
+                  Goto: ()=>{},
+                  AccessText: 'เข้าสู่ระบบด้วย Facebook',
+                  ButColor: Color.fromRGBO(66, 103, 178, 10)),
+              Space(EmtySpace: MediaQuery.of(context).size.height * 0.02),
+              ButtonAccess(
                 Goto: ()=>{},
-                AccessText: 'เข้าสู่ระบบด้วย Google',
-                ButColor: Color.fromRGBO(245, 9, 9, 10)),
-            Space(EmtySpace: MediaQuery.of(context).size.height * 0.01),
-            ButtonAccess(
-                Goto: ()=>{},
-                AccessText: 'เข้าสู่ระบบด้วย Facebook',
-                ButColor: Color.fromRGBO(66, 103, 178, 10)),
-            Space(EmtySpace: MediaQuery.of(context).size.height * 0.01),
-            ButtonAccess(
-              Goto: ()=>{},
-              AccessText: 'เข้าสู่ระบบด้วย Line',
-              ButColor: Color.fromRGBO(3, 195, 2, 10),
-            ),
+                AccessText: 'เข้าสู่ระบบด้วย Line',
+                ButColor: Color.fromRGBO(3, 195, 2, 10),
+              ),
 
-          ],
+            ],
+          ),
         ),
       ),
     );
   }
 }
+
