4151b6c7534c5b46cc0588d40e7ee0526de81ef8
KEI <samartketrahong@gmail.com>
2021-03-07 18:11:12 +0700

    version : 7/3/63 - Into App

---
+++ lib/Into/MainInto.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import '../Login/LoginMain.dart';
+class FristPage extends StatefulWidget {
+  @override
+  _FirstPageState createState() => _FirstPageState();
+}
+
+class _FirstPageState extends State<FristPage> {
+  @override
+  void initState() {
+    timerLinkPage(context);
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white12,
+      body: Center(
+          child: Container(
+        child: Image.asset(
+          'res/logo.png',
+        ),
+      )),
+    );
+  }
+}
+Future timerLinkPage (BuildContext context) {
+  return Future.delayed(Duration(seconds: 3),(){
+    Navigator.push(context, MaterialPageRoute(builder: (context) => MainLogin() ));
+  });
+}
---
+++ lib/Login/LoginMain.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class MainLogin extends StatefulWidget {
+  @override
+  _MainLoginState createState() => _MainLoginState();
+}
+
+class _MainLoginState extends State<MainLogin> {
+  @override
+  Widget build(BuildContext context) {
+   return Scaffold(body: Container(),);
+  }
+}
