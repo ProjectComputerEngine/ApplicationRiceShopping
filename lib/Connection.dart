dfc6a3c7b2dfb041680cd399face622df1590026
KEI <samartketrahong@gmail.com>
2021-03-13 20:19:22 +0700

    version : 10/3/63 - Connection time out insert

--- lib/Connection.dart
+++ lib/Connection.dart
@@ -12,28 +12,33 @@ class Connection {
         return 'ok';
       }
       else{
+        showMyDialog(context, 'การเชื่อมต่อล้มเหลว กรุณาลองใหม่อีกครั้ง !');
         return null;
-        // showMyDialog(context, 'การเชื่อมต่อล้มเหลว กรุณาลองใหม่อีกครั้ง !');
       }
     }
     else {
+      showMyDialog(context, 'การเชื่อมต่อล้มเหลว กรุณาลองใหม่อีกครั้ง !');
       return null;
-       // showMyDialog(context, 'การเชื่อมต่อล้มเหลว กรุณาลองใหม่อีกครั้ง !');
     }
   }
   Future<String> connectLogin({String username, String password, String device, String time, String ip,BuildContext context}) async {
     String url = 'http://$server/login/Login2Database.php?username=$username&password=$password&device=$device&time=$time&ip=$ip';
-    var response = await get(url);
+    var response = await get(url).timeout(Duration(seconds: 5), onTimeout: (){
+      showMyDialog(context, 'การเชื่อมต่อล้มเหลว กรุณาลองใหม่อีกครั้ง !');
+      return null;
+    });
     if (response.statusCode == 200){
       if(response.body.isNotEmpty){
         return response.body;
       }
       else {
         showMyDialog(context, 'การเชื่อมต่อล้มเหลว กรุณาลองใหม่อีกครั้ง !');
+        return null;
       }
     }
     else {
       showMyDialog(context, 'การเชื่อมต่อล้มเหลว กรุณาลองใหม่อีกครั้ง !');
+      return null;
     }
   }
 
